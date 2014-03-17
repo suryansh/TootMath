7d15f4f18a042d1cad0acd62bf4929c851bcbba4
Pavan.Adavi@gmail.com <pavan.adavi@gmail.com>
2014-03-16 18:16:43 -0700

    Initial Commit

---
+++ TootMath/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TootMath
+//
+//  Created by Pavan Adavi on 3/16/14.
+//  Copyright (c) 2014 Pavan Adavi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ TootMath/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TootMath
+//
+//  Created by Pavan Adavi on 3/16/14.
+//  Copyright (c) 2014 Pavan Adavi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
